cc6fd59976b289e0e5323fac0b4c0d89646706e0
minseoky <minseok184@gmail.com>
2024-05-07 11:02:37 +0900

    feat(*): MainService 기본 틀 작성

---
+++ src/main/java/info/dexplore/dexplore/dto/response/main/MuseumResponseDto.java
@@ -0,0 +1,11 @@
+package info.dexplore.dexplore.dto.response.main;
+
+import info.dexplore.dexplore.dto.response.ResponseDto;
+
+public class MuseumResponseDto extends ResponseDto {
+
+    public MuseumResponseDto() {
+        super();
+    }
+
+}
