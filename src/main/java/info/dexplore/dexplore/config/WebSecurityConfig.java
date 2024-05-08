3bef0094c86715e83a0226887879399ddd6e5422
minseoky <minseok184@gmail.com>
2024-05-09 06:05:25 +0900

    fix(WebSecurityConfig.java): /api/v1/user/**경로에 ADMIN 접근 가능하도록 수정

--- src/main/java/info/dexplore/dexplore/config/WebSecurityConfig.java
+++ src/main/java/info/dexplore/dexplore/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig{
                 )
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/", "/api/v1/auth/**", "/oauth2/**").permitAll()
-                        .requestMatchers("/api/v1/user/**").hasRole("USER")
+                        .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
