87638e049d3c542cf40f14a14e28a8f408ce0c32
minseoky <minseok184@gmail.com>
2024-05-09 10:10:28 +0900

    feat(*): 박물관 정보 update 관련 api 추가

---
+++ src/main/java/info/dexplore/dexplore/dto/request/main/UpdateMuseumRequestDto.java
@@ -0,0 +1,62 @@
+package info.dexplore.dexplore.dto.request.main;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UpdateMuseumRequestDto {
+
+    @NotNull
+    private Long museumId;
+
+    @NotBlank
+    private String museumName;
+
+    @NotBlank
+    private String entPrice;
+
+    @NotBlank
+    @Email
+    private String museumEmail;
+
+    @NotBlank
+    private String startTime;
+
+    @NotBlank
+    private String endTime;
+
+    @NotBlank
+    private String closingDay;
+
+    @Size(max = 2000, message = "2000자 제한")
+    private String description;
+
+    @NotBlank
+    private String phone;
+
+    //이하 공간정보
+
+    @NotNull
+    private BigDecimal latitude;
+
+    @NotNull
+    private BigDecimal longitude;
+
+    @NotBlank
+    private String level;
+
+    private BigDecimal edgeLatitude1;
+    private BigDecimal edgeLongitude1;
+    private BigDecimal edgeLatitude2;
+    private BigDecimal edgeLongitude2;
+
+}
---
+++ src/main/java/info/dexplore/dexplore/dto/response/main/UpdateMuseumResponseDto.java
@@ -0,0 +1,24 @@
+package info.dexplore.dexplore.dto.response.main;
+
+import info.dexplore.dexplore.common.ResponseCode;
+import info.dexplore.dexplore.common.ResponseMessage;
+import info.dexplore.dexplore.dto.response.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class UpdateMuseumResponseDto extends ResponseDto {
+
+    public UpdateMuseumResponseDto() {
+        super();
+    }
+
+    public static ResponseEntity<ResponseDto> idNotFound() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.ID_NOT_FOUND, ResponseMessage.ID_NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseBody);
+    }
+
+    public static ResponseEntity<ResponseDto> museumNotFound() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.MUSEUM_NOT_FOUND, ResponseMessage.MUSEUM_NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseBody);
+    }
+}
