225c6ea5c32433ddf7ac5ab23c58ccbdae7825f1
minseoky <minseok184@gmail.com>
2024-05-15 07:02:04 +0900

    feat(*): 가까운 N개의 작품 조회 api 추가(USER)

---
+++ src/main/java/info/dexplore/dexplore/dto/request/main/user/GetNearestNArtsRequestDto.java
@@ -0,0 +1,27 @@
+package info.dexplore.dexplore.dto.request.main.user;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class GetNearestNArtsRequestDto {
+
+    @NotNull
+    private Long museumId;
+
+    @NotNull
+    private BigDecimal latitude;
+
+    @NotNull
+    private BigDecimal longitude;
+
+    @NotNull
+    private int amount; //N
+
+}
