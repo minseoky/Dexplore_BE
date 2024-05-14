b058d60212defd4100c323708c5dd30a3c32c4d0
minseoky <minseok184@gmail.com>
2024-05-15 06:18:52 +0900

    feat(*): 작품정보 조회(admin) api 추가

---
+++ src/main/java/info/dexplore/dexplore/dto/request/main/admin/GetArtsRequestDto.java
@@ -0,0 +1,16 @@
+package info.dexplore.dexplore.dto.request.main.admin;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class GetArtsRequestDto {
+
+    @NotNull
+    private Long museumId;
+
+}
